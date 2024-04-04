b558d50a4951130e1865bf7edfe61f4d1295daf1
Duan <josiasduan@gmail.com>
2024-04-04 18:10:22 -0300

    First commit

---
+++ src/components/Header/Logo.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+
+function Logo() {
+  return (
+    <Link href="/" className="flex items-center text-dark">
+      <div className="w-16 rounded-full overflow-hidden border border-solid border-dark">
+        <Image
+          src="/profile-img.png"
+          alt="profile"
+          width={100}
+          height={100}
+          className="w-full h-auto rounded-full"
+        />
+      </div>
+      <span className="font-bold text-xl ml-4">DuDevJourney</span>
+    </Link>
+  );
+}
+
+export default Logo;
---
+++ src/components/Header/index.js
@@ -0,0 +1,44 @@
+import Logo from "./Logo";
+import Link from "next/link";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { IoMdSunny } from "react-icons/io";
+function Header() {
+  return (
+    <header className="w-full p-4 px-10 flex items-center justify-between">
+      <Logo />
+      <nav
+        className="
+        fixed top-6 right-1/2 translate-x-1/2 w-max 
+        py-3 px-8 border border-solid 
+        border-dark rounded-full 
+        font-medium capitalize flex items-center
+        bg-light/80 backdrop-blur-sm
+        "
+      >
+        <Link className="mr-2" href="/">
+          Home
+        </Link>
+        <Link className="mr-2" href="/about">
+          About
+        </Link>
+        <Link className="mr-2" href="/contact">
+          Contact
+        </Link>
+        <button><IoMdSunny fontSize={25} className="animate-spin-slow"/></button>
+      </nav>
+      <div className="flex gap-3">
+        <a href="http://ex.com" className="text-lg">
+          <FaGithub  fontSize={30}/>
+        </a>
+        <a href="http://ex.com">
+          <FaInstagram fontSize={30}/>
+        </a>
+        <a href="http://ex.com">
+          <FaLinkedin fontSize={30}/>
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
