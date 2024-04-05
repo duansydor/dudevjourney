a147ffd44e50496f5603c476d81c079f7b2252ea
Duan <josiasduan@gmail.com>
2024-04-05 18:42:39 -0300

    Day 2 - learn how to render markdown using nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
-
+import {allBlogs} from "contentlayer/generated"
+import HomeCoverSection from '@/components/Home/HomeCoverSection'
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center">
-      
+    <main className="">
+      <HomeCoverSection blogs={allBlogs}/>
     </main>
   )
 }
