19bf69339d9cdae086104b2e86f75b7e677e5703
Duan Josias Sydor <42321964+duansydor@users.noreply.github.com>
2024-04-08 15:43:34 -0300

    Atualizar o layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,6 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer.js";
 import siteMetadata from "@/utils/siteMetaData";
 import { ThemeProvider } from "next-themes";
-import Script from "next/script";
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
@@ -64,13 +63,6 @@ export default function RootLayout({ children }) {
           "font-mr bg-light dark:bg-dark"
         )}
       >
-        {/* <Script id="theme-switcher" strategy="beforeInteractive">
-          {`if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-  } else {
-    document.documentElement.classList.remove('dark')
-  }`}
-        </Script> */}
         <Header />
         {children}
         <Footer />
